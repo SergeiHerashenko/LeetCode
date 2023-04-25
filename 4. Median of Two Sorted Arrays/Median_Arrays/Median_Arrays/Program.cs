8c73d7d5a95251c2337e5b554dccd0b0e85906c8
sergei Herashenko <46726784+SergeiHerashenko@users.noreply.github.com>
2023-04-25 20:39:31 +0300

    LeetCode

---
+++ 4. Median of Two Sorted Arrays/Median_Arrays/Median_Arrays/Program.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+ * The overall run time complexity should be O(log (m+n)).
+ * 
+ * 
+ * Example 1:
+ * Input: nums1 = [1,3], nums2 = [2]
+ * Output: 2.00000
+ * Explanation: merged array = [1,2,3] and median is 2.
+ * 
+ * 
+ * Example 2:
+ * Input: nums1 = [1,2], nums2 = [3,4]
+ * Output: 2.50000
+ * Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+ */
+using System;
+using System.Globalization;
+using System.Linq;
+
+namespace Median_Arrays
+{
+    internal class Program
+    {
+        // This method takes two sorted integer arrays as input and returns the median of their merged array
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            // Declare variables for the result and the merged array
+            double Result;
+            int[] ResultArray = nums1.Concat(nums2).ToArray();
+            // Sort the merged array in ascending order
+            Array.Sort(ResultArray);
+
+            // If the length of the merged array is even, calculate the median by taking the average of the middle two numbers
+            if (ResultArray.Length % 2 == 0)
+            {
+                int i;
+                int j;
+                i = ResultArray[ResultArray.Length / 2 - 1];
+                j = ResultArray[ResultArray.Length / 2];
+                Result = ((i + j) / 2.0);
+            }
+            // If the length of the merged array is odd, the median is the middle number
+            else
+            {
+                Result = ResultArray[ResultArray.Length / 2];
+            }
+            // Return the calculated median
+            return Result;
+        }
+        static void Main(string[] args)
+        {
+            // Prompt the user to enter the first array
+            Console.Write("Enter first mass: ");
+            string[] ArrayFirst = Console.ReadLine().Split(' ');
+
+            // Prompt the user to enter the second array
+            Console.Write("Enter second mass: ");
+            string[] ArraySecond = Console.ReadLine().Split(' ');
+
+            // Convert the input strings into integer arrays
+            int[] ArrayOne = new int[ArrayFirst.Length];
+            for(int i = 0; i < ArrayFirst.Length; i++)
+            {
+                ArrayOne[i] = int.Parse(ArrayFirst[i]);
+            }
+
+            int[] ArrayTwo = new int[ArraySecond.Length];
+            for (int i = 0; i < ArraySecond.Length; i++)
+            {
+                ArrayTwo[i] = int.Parse(ArraySecond[i]);
+            }
+
+            // Create an instance of the Program class and call the FindMedianSortedArrays method with the two integer arrays
+            Program program = new Program();
+            double Result = program.FindMedianSortedArrays(ArrayOne, ArrayTwo);
+
+            // Print the calculated median to the console
+            Console.WriteLine(Result);
+        }
+    }
+}
---
+++ 5. Longest Palindromic Substring/Longest_Palindromic/Longest_Palindromic/Program.cs
@@ -0,0 +1,87 @@
+﻿/*Given a string s, return the longest palindromic substring in s.
+ * 
+ * 
+ * Example 1:
+ * Input: s = "babad"
+ * Output: "bab"
+ * Explanation: "aba" is also a valid answer.
+ * 
+ * 
+ * Example 2:
+ * Input: s = "cbbd"
+ * Output: "bb"
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Longest_Palindromic
+{
+    internal class Program
+    {
+        // Method for finding the longest palindrome in the given string
+        public string LongestPalindrome(string s)
+        {
+            // Initializing a string variable named "Result" with an empty string
+            string Result = "";
+
+            // Initializing an integer variable named "counter" with a value of zero
+            int counter = 0;
+
+            // Looping through the input string until it becomes empty
+            while (s.Length > 0)
+            {
+                // Initializing a string variable named "Stroka" with the first character of the input string
+                string Stroka = Convert.ToString(s[counter]);
+                // Looping through the input string until the end or until the current substring is not a palindrome
+                while (counter != s.Length)
+                {
+                    // Checking if the current substring is a palindrome
+                    if (Stroka == new string(Stroka.Reverse().ToArray()))
+                    {
+                        // If the current substring is a palindrome and its length is greater than the length of the current result,
+                        // update the result with the current substring
+                        if (Result.Length < Stroka.Length)
+                        {
+                            Result = Stroka;
+                        }
+                        // Incrementing the counter and adding the next character to the current substring (if it exists)
+                        counter++;
+                        if (counter != s.Length)
+                        {
+                            Stroka += Convert.ToString(s[counter]);
+                        }
+                    }
+                    // If the current substring is not a palindrome, increment the counter and add the next character to the current substring (if it exists)
+                    else
+                    {
+                        counter++;
+                        if (counter != s.Length)
+                        {
+                            Stroka += Convert.ToString(s[counter]);
+                        }
+                    }
+                }
+
+                // Resetting the counter and removing the first character from the input string
+                counter = 0;
+                s = s.Remove(0, 1);
+            }
+            // Returning the result
+            return Result;
+        }
+        static void Main(string[] args)
+        {
+            // Ask the user for a string to search for a palindrome
+            Console.Write("Enter a string: ");
+            string userString = Console.ReadLine();
+
+            // Create an object of the Program class and call the LongestPalindrome method to find the longest palindromic substring
+            Program program = new Program();
+            string Result = program.LongestPalindrome(userString);
+
+            // Display the longest palindromic substring
+            Console.WriteLine(Result);
+        }
+    }
+}
